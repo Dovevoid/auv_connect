0683340048f564d4674e60b9dd21b7eeefacfa42
dovevoid <1935029729@qq.com>
2024-05-13 16:28:01 +0800

    master

--- build/auv_chat/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dove/auv_ws/src/auv_chat
-# Build directory: /home/dove/auv_ws/build/auv_chat
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/nuc_tcp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dove/auv_ws/src/nuc_tcp
-# Build directory: /home/dove/auv_ws/build/nuc_tcp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
